74ee115f4bfb41077d78de3c19ce272dd0ee7123
hrdip <148678933+hrdip@users.noreply.github.com>
2023-11-23 23:58:26 +0800

    custom user model
    custom user model by AbstractBaseUser

---
+++ core/accounts/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin)
+from django.utils.translation import ugettext_lazy as _
+# Create your models here.
+
+class UserManager(BaseUserManager):
+    ''' custom user model manager where email is the uniqu identifiers
+    for authentication instead of username
+    '''
+
+    def create_user(self,email,password,**extra_fields):
+        '''
+        create and sava a user with the given 
+        emial and password and extra data
+        '''
+        if not email:
+            raise ValueError(_('the email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self,email,password,**extra_fields):
+        '''
+        create and sava a superuser with the given 
+        emial and password
+        '''
+        extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_active',True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True'))
+        return self.create_user(email,password,**extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    ''' 
+    custom User Model for our app
+    '''
+
+    email = models.EmailField(max_length=255, unique=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    #is_verified = models.BooleanField(default=False)
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.email
+     
--- core/core/settings.py
+++ core/core/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    #'blog',
+    
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# user manager configuration
+AUTH_USER_MODEL = 'accounts.User'
