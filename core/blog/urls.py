d0f2b758ffd38fe86f8e018b817f6e9781f08264
hrdip <148678933+hrdip@users.noreply.github.com>
2023-11-25 02:00:02 +0800

    ListView

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -9,10 +9,11 @@ app_name = "blog"
 urlpatterns = [
     
     path('/fbv-index', indexView, name="fbv-index"),
-    path('/cbv-index', TemplateView.as_view(template_name='index.html', extra_content={"name":"hossein"})),
-    path('/ccbv-index', views.IndexView.as_view(), name="ccbv-index"),
+    #path('/cbv-index', TemplateView.as_view(template_name='index.html', extra_content={"name":"hossein"})),
+    path('/cbv-index', views.IndexView.as_view(), name="cbv-index"),
     #path('/go-to-hiva', RedirectView.as_view(url ='https://hiva-trading.com/'), name="redirect-to-hiva"),
     path('/go-to-index', RedirectView.as_view(pattern_name ="blog:cbv-index"), name="redirect-to-index"),
-    #path ('/go-to-hiva', redirectToHiva, name="redirect-to-hiva"),
-    path ('/go-to-hiva', views.RedirectToHiva.as_view(), name="redirect-to-hiva"),
+    #path('/go-to-hiva', redirectToHiva, name="redirect-to-hiva"),
+    path('/go-to-hiva', views.RedirectToHiva.as_view(), name="redirect-to-hiva"),
+    path('/post/', views.PostList.as_view(), name='post-list'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic import ListView
 from .models import Post
 # Create your views here.
 
@@ -13,7 +14,7 @@ def indexView(request):
     context = {"name": name}
     return render(request,"index.html",context)
 
-
+# Custom Class Base View for TemplateView
 class IndexView(TemplateView):
     """
     a class based view to show index page
@@ -26,12 +27,31 @@ class IndexView(TemplateView):
         context["posts"] = Post.objects.all()
         return context
 
-''' Function Base View for redirect
+#Function Base View for RedirectView
+'''
 from django.shortcuts import redirect
 def redirectToHiva(request):
     return redirect('https://hiva-trading.com')
 '''
 
+# Custom Class Base View for RedirectView
 class RedirectToHiva(RedirectView):
     url = 'https://hiva-trading.com'
     
+
+
+# Custom Class Base View for ListView
+class PostList(ListView):
+    # model = Post
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    ordering = '-id'
+
+    ''' 
+    insted of model and queryset we can customize with
+    this function
+    '''
+    # def get_queryset(self):
+    #     posts = Post.objects.filter(status=True)
+    #     return posts
