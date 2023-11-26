493c295001a4c3ee296499965310f289e6ce6a3f
hrdip <148678933+hrdip@users.noreply.github.com>
2023-11-26 17:24:14 +0800

    install django rest_framework

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import indexView
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
@@ -17,7 +17,9 @@ urlpatterns = [
     path('/go-to-hiva', views.RedirectToHiva.as_view(), name="redirect-to-hiva"),
     path('/post/', views.PostListView.as_view(), name='post-list'),
     path('/post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path ('/post/create/', views.PostCreateView.as_view(), name='post-create'),
-    path ('/post/<int:pk>/edit/', views.PostEditView.as_view(), name='post-edit'),
-    path ('/post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'), 
+    path('/post/create/', views.PostCreateView.as_view(), name='post-create'),
+    path('/post/<int:pk>/edit/', views.PostEditView.as_view(), name='post-edit'),
+    path('/post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('/api/v1/', include('blog.api.v1.urls')),
+
 ]
--- core/core/settings.py
+++ core/core/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'accounts',
     'blog',
     'website',
-    
+    'rest_framework',
+
 ] 
 
 MIDDLEWARE = [
