f641bcf4f1d7b74cdde77d0505ebc1506289f8fe
hrdip <148678933+hrdip@users.noreply.github.com>
2023-11-28 01:10:09 +0800

    get_absolute_uri

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -12,10 +12,20 @@ class PostSerializer(serializers.Serializer):
 
 # model doe ModelSerializer
 class PostSerializer(serializers.ModelSerializer):
-
+    #content = serializers.ReadOnlyField()
+    #content =serializers.CharField(read_only=True)
+    snippet = serializers.ReadOnlyField(source='get_snippet')
+    relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
+    absolute_url = serializers.SerializerMethodField()
     class Meta:
         model = Post
-        fields = ['id', 'author', 'title', 'content', 'category', 'status', 'created_date', 'published_date']
+        fields = ['id', 'author', 'title', 'content', 'category', 'status','snippet', 'relative_url', 'absolute_url', 'created_date', 'published_date']
+        #read_only_fields = ['content']
+
+    def get_absolute_url(self,obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri()
+
 
 
 class CategorySerializer(serializers.ModelSerializer):
