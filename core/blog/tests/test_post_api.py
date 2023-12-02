ee8ffe9cb43273fc382fb025119861d8a80056cd
hrdip <148678933+hrdip@users.noreply.github.com>
2023-12-02 23:16:35 +0800

    Update test_post_api.py

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -14,45 +14,48 @@ def api_client():
 @pytest.fixture
 def common_user():
     user = User.objects.create_user(
-        email="admin@example.com",password="a/@1234567",is_verified=True
+        email="admin@example.com", password="a/@1234567", is_verified=True
     )
     return user
 
 
 @pytest.mark.django_db
 class TestPostApi:
-    def test_get_post_response_200_status(self,api_client):
+    def test_get_post_response_200_status(self, api_client):
         url = reverse("blog:api-v1:post-list")
         response = api_client.get(url)
         assert response.status_code == 200
-    def test_create_post_response_403_status(self,api_client):
+
+    def test_create_post_response_403_status(self, api_client):
         url = reverse("blog:api-v1:post-list")
         data = {
-            "title":"test",
-            "content":"description",
-            "status":True,
-            "published_date":datetime.now(),
+            "title": "test",
+            "content": "description",
+            "status": True,
+            "published_date": datetime.now()
         }
-        response = api_client.post(url,data)
+        response = api_client.post(url, data)
         assert response.status_code == 403
-    def test_create_post_response_201_status(self,api_client,common_user):
+
+    def test_create_post_response_201_status(self, api_client, common_user):
         url = reverse("blog:api-v1:post-list")
         data = {
-            "title":"test",
-            "content":"description",
-            "status":True,
-            "published_date":datetime.now(),
+            "title": "test",
+            "content": "description",
+            "status": True,
+            "published_date": datetime.now()
         }
         user = common_user
         api_client.force_authenticate(user=user)
-        response = api_client.post(url,data)
+        response = api_client.post(url, data)
         assert response.status_code == 201
+
     def test_create_post_invalid_data_response_400_status(
-        self,api_client,common_user
+        self, api_client, common_user
     ):
         url = reverse("blog:api-v1:post-list")
-        data = {"title":"test","published_date":datetime.now()}
+        data = {"title": "test", "published_date": datetime.now()}
         user = common_user
         api_client.force_authenticate(user=user)
-        response = api_client.post(url,data)
+        response = api_client.post(url, data)
         assert response.status_code == 400
