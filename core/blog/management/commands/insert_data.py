a66979c198b1bf2b5e04979917b456e3230b5cae
hrdip <148678933+hrdip@users.noreply.github.com>
2023-12-03 15:51:41 +0800

    Reformated test for flake8

--- core/blog/management/commands/insert_data.py
+++ core/blog/management/commands/insert_data.py
@@ -16,15 +16,16 @@ category_list = [
     "AcidResistance"
 ]
 
+
 class Command(BaseCommand):
-    help = 'inserting dummy data'
+    help = 'inserting_dummy_data'
 
     def __init__(self, *args, **kwargs):
         super(Command, self).__init__(*args, **kwargs)
         self.fake = Faker()
 
     def handle(self, *args, **kwargs):
-        user = User.objects.create_user(email=self.fake.email(),password="Test@123456")
+        user = User.objects.create_user(email=self.fake.email(), password="Test@123456")
         profile = Profile.objects.get(user=user)
         profile.first_name = self.fake.first_name()
         profile.last_name = self.fake.last_name()
@@ -37,10 +38,10 @@ class Command(BaseCommand):
 
         for _ in range(10):
             Post.objects.create(
-                author = profile,
-                title = self.fake.paragraph(nb_sentences=1),
-                content = self.fake.paragraph(nb_sentences=10),
-                status = random.choice([True,False]),
-                category = Category.objects.get(name=random.choice(category_list)),
-                published_date = datetime.now()
+                author=profile,
+                title=self.fake.paragraph(nb_sentences=1),
+                content=self.fake.paragraph(nb_sentences=10),
+                status=random.choice([True, False]),
+                category=Category.objects.get(name=random.choice(category_list)),
+                published_date=datetime.now()
             )
