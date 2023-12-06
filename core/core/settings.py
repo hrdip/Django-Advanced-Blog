f8fc8173a735c9faacad885561dde379d734733a
hrdip <148678933+hrdip@users.noreply.github.com>
2023-12-06 23:25:05 +0800

    stage

--- core/core/settings.py
+++ core/core/settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.sqlite3",
-#        "NAME": BASE_DIR / "db.sqlite3",
-#    }
-#}
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "test_db",
-        "USER": "admin",
-        "PASSWORD": "Hrdip@2010",
-        "HOST": "172.23.0.2",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.postgresql",
+#        "NAME": "test_db",
+#        "USER": "admin",
+#        "PASSWORD": "Hrdip@2010",
+#        "HOST": "172.23.0.2",
+#        "PORT": "5432",
+#    }
+#}
 
 
 # Password validation
