391c2fe37fcb72c80bfdf9420db4c2c75085762f
hrdip <148678933+hrdip@users.noreply.github.com>
2023-12-06 23:11:02 +0800

    ses

--- core/core/settings.py
+++ core/core/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         "NAME": "test_db",
         "USER": "admin",
         "PASSWORD": "Hrdip@2010",
-        "HOST": "localhost",
+        "HOST": "172.23.0.2",
         "PORT": "5432",
     }
 }
