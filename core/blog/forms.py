e451ef45c8c5e6699c157f6f09ef5f7abc5bb14f
Hossein L. Raei <hrdip.2018@gmail.com>
2024-05-29 22:07:57 +0800

    form style

--- core/blog/forms.py
+++ core/blog/forms.py
@@ -6,3 +6,12 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ("title", "content", "status", "category", "published_date")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["title"].widget.attrs['class'] = 'form-control'
+        self.fields["content"].widget.attrs['class'] = 'form-control'
+        self.fields["status"].widget.attrs['class'] = 'form-check-input mb-3'
+        self.fields["category"].widget.attrs['class'] = 'form-control'
+        self.fields["published_date"].widget.attrs['class'] = 'form-control'
+        self.fields["published_date"].widget.attrs['type'] = 'datetime-local'
